a3f102e7f51bff6628515011ec4afde053052feb
Steven Birkner <stevebirk36@gmail.com>
2014-03-10 11:06:20 -0400

    ADD: photo.ejs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
+var photos = require('./routes/photo.js');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -29,6 +30,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/', photos.list);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
